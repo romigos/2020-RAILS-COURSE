0490993f34d3718faa4bd9625267dd7e6fc40ea7
romigos <bolistovskiyroman@gmail.com>
2020-06-16 15:22:32 +0300

    3 lesson of course make migration

--- db/migrate/20200615144548_create_items.rb
+++ db/migrate/20200615144548_create_items.rb
@@ -1,7 +1,10 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.float :price
+      t.string :name
+      t.boolean :real
+      t.float :weight
       t.timestamps
     end
   end
