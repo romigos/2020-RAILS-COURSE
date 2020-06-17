d1823b58d4694514ae50e5c73a9e3bb1c902166c
romigos <bolistovskiyroman@gmail.com>
2020-06-17 17:30:16 +0300

    5 lesson of course add validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Item < ApplicationRecord
+  validates :price, numericality: {greater_than: 0}, allow_nil: true
+  validates :price, :description, presence: true
 end
