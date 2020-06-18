1d58ebf26e3dddd527dcda2ab95fb4416be29143
romigos <bolistovskiyroman@gmail.com>
2020-06-18 16:58:30 +0300

    7 lesson of course callbacks

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class Item < ApplicationRecord
-  validates :price, numericality: {greater_than: 0}, allow_nil: true
+  validates :price, numericality: {greater_than: 0, allow_nil: true}
   validates :price, :description, presence: true
+
+  after_initialize { p 'initialize' } # Item.new
+  after_save { p 'save' } # Item.save
+  after_create { p 'create' } # Item.create
+  after_update { p 'update' } # Item.update
+  after_destroy { p 'destroy' } # Item.destroy
 end
