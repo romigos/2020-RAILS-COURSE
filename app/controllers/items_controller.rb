98cc9176f2b922b4f715df517b54d9d2cbc8477b
romigos <bolistovskiyroman@gmail.com>
2020-06-22 17:06:16 +0300

    9 lesson of course create params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,23 @@
 # frozen_string_literal: true
 
 class ItemsController < ApplicationController
+  layout false
+
   def index
     @items = Item.all
     render text: @items.map do |i|
       "#{i.name}:#{i.price}:#{i.description}"
     end
+  end
+
+  def create
+    Item.create(items_params)
+    render body: 'created'
+  end
+
+  private
+
+  def items_params
+    params.permit(:name, :price)
   end
 end
